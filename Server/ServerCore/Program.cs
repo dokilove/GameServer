2dc70e13c14ab197b80158da16eaa3903215a39d
dokilove2@gmail.com <tulmooner2@>
2023-05-04 01:37:23 +0900

    섹션1 / 멀티쓰레드 개론      / 쓰레드 생성

--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -1,10 +1,53 @@
-﻿namespace ServerCore
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ServerCore
 {
-    internal class Program
+    class Program
     {
+        static void MainThread(object state)
+        {
+            for (int i = 0; i < 5; ++i)
+            {
+                Console.WriteLine("Hello Thread!");
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            ThreadPool.SetMinThreads(1, 1);
+            ThreadPool.SetMaxThreads(5, 5);
+
+            for (int i = 0; i < 5; ++i)
+            {
+                Task t = new Task(() => { while (true) { } }, TaskCreationOptions.LongRunning);
+                t.Start();
+            }
+
+            //for (int i =0; i< 4; ++i)
+            //{
+            //    ThreadPool.QueueUserWorkItem((obj) => { while (true) { } });
+            //}
+            ThreadPool.QueueUserWorkItem(MainThread);
+
+            //for (int i = 0; i < 1000; ++i)
+            //{
+            //    Thread t = new Thread(MainThread);
+            //    //t.Name = "Test Thread";
+            //    t.IsBackground = true;
+            //    t.Start();
+            //}
+
+            //Console.WriteLine("Waiting for Thread!");
+            //t.Join();
+
+            //Console.WriteLine("Hello, World!");
+
+            while (true)
+            {
+
+            }
         }
     }
 }
