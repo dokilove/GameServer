6cc3e75551b39d9e4eec9a28675093038c369d3c
dokilove2@gmail.com <tulmooner2@>
2023-05-08 21:55:07 +0900

    섹션1 / 멀티쓰레드 개론      / 캐시 이론

--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -6,35 +6,32 @@ namespace ServerCore
 {
     class Program
     {
-        volatile static bool _stop = false;
-
-        static void TheadMain()
-        {
-            Console.WriteLine("쓰레드 시작");
-
-            while (_stop == false) 
-            {
-                // 누군가가 stop 신호를 해주기를 기다린다
-            }
-
-            Console.WriteLine("쓰레드 종료!");
-        }
-
         static void Main(string[] args)
         {
-            Task t = new Task(TheadMain);
-            t.Start();
+            // 후자는 
+            // 5 * 5 배열일때
+            // [][][][][] [][][][][] [][][][][] [][][][][]
+            //  1          2          3          4의 순서로 채워져서 느려진다
+            int[,] arr = new int[10000, 10000];
 
-            Thread.Sleep(1000);
+            {
+                long now = DateTime.Now.Ticks;
+                for (int y = 0; y < 10000; y++)
+                    for (int x = 0; x < 10000; x++)
+                        arr[y, x] = 1;
 
-            _stop = true;
+                long end = DateTime.Now.Ticks;
+                Console.WriteLine($"(y,x) 순서 걸린 시간 { end - now}");
+            }
 
-            Console.WriteLine("Stop 호출");
-            Console.WriteLine("종료 대기중");
-
-            t.Wait();
-
-            Console.WriteLine("종료 성공");
+            {
+                long now = DateTime.Now.Ticks;
+                for (int y = 0; y < 10000; y++)
+                    for (int x = 0; x < 10000; x++)
+                        arr[x, y] = 1;
+                long end = DateTime.Now.Ticks;
+                Console.WriteLine($"(x,y) 순서 걸린 시간 {end - now}");
+            }
         }
     }
 }
