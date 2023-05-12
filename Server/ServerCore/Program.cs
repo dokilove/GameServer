daf26745b5c359105ad7e42ef1c7d4881db9d289
dokilove2@gmail.com <tulmooner2@>
2023-05-12 18:47:02 +0900

    섹션1 / 멀티쓰레드 개론      / Lock 기초

--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -8,32 +8,50 @@ namespace ServerCore
     {
 
         static int number = 0;
+        static object _obj = new object();
+
 
         static void Thread_1()
         {
-            // atomic = 원자성
-
-
-
-            for (int i = 0; i < 100000000; i++)
+            for (int i = 0; i < 10000000; i++)
             {
-                // All or Nothing
-                int prev = number;
-                Interlocked.Increment(ref number);
+                // 상호배제 Mutual Exclusive
+                lock(_obj)
+                {
+                    number++;
+                }
 
-                int next = number;
+                //try
+                //{
+                //    Monitor.Enter(_obj); // 문을 잠그는 행위
+                //    number++;
 
+                //    return;
+                //}
+                //finally // finally는 무조건 한번 실행된다
+                //{
+                //    Monitor.Exit(_obj); // 잠금을 풀어준다
+                //}
 
-                Console.WriteLine(prev + " " + next + " " + (prev + 1 == next));
+                //// CriticalSection std:mutex
+                //Monitor.Enter(_obj); // 문을 잠그는 행위
+                //{
+                //    number++;   
+                //}
+                //Monitor.Exit(_obj); // 잠금을 풀어준다
             }
         }
+
+        // 데드락 DeadLock
 
         static void Thread_2()
         {
             for (int i = 0; i < 10000000; i++)
             {
-                Interlocked.Decrement(ref number);
-
+                lock (_obj)
+                {
+                    number--;
+                }
             }
         }
         static void Main(string[] args)
