4fbc453a6cc470c922e0267c99068ac3931c4ee4
dokilove2@gmail.com <tulmooner2@>
2023-05-22 18:53:34 +0900

    섹션 1 : ReaderWriterLock 구현연습

---
+++ Server/ServerCore/Lock.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization.Formatters;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerCore
+{
+    // 재귀적 락을 허용할지 (Yes) WriteLock->WriteLock OK, WriteLock->ReadLock OK, ReadLock->WriteLock NO    
+    // 스핀락 정책 (5000번 -> Yield)
+    class Lock
+    {
+        const int EMPTY_FLAG = 0x00000000;
+        const int WRITE_MASK = 0x7FFF0000;
+        const int READ_MASK = 0x0000FFFF;
+        const int MAX_SPIN_COUNT = 5000;
+
+        // 32비트
+        // [Unused(1)] [WriteThreadId(15)] [ReadCount(16)]
+        int _flag = EMPTY_FLAG;
+        int _writeCount = 0;
+
+        public void WriteLock()
+        {
+            // 동일 쓰레드가 WriteLock을 이미 획득하고 있는지 확인
+            int lockThreadId = (_flag & WRITE_MASK) >> 16;
+            if (Thread.CurrentThread.ManagedThreadId == lockThreadId) 
+            {
+                _writeCount++;
+                return;
+            }
+
+            // 아무도 WriteLock or ReadLock을 획득하고 있지 않을 때, 경합해서 소유권을 얻는다
+            int desired = (Thread.CurrentThread.ManagedThreadId << 16) & WRITE_MASK;
+            while(true)
+            {
+                for (int i=0; i <MAX_SPIN_COUNT; i++) 
+                {
+                    if (Interlocked.CompareExchange(ref _flag, desired, EMPTY_FLAG) == EMPTY_FLAG)
+                    {
+                        _writeCount = 1;
+                        return;
+                    }
+                    // 시도를 해서 성공하면 return
+                    // 의사코드
+                    // if (_flag == EMPTY_FLAG)                    
+                    //    _flag = desired;                    
+                }
+
+                Thread.Yield();
+            }
+        }
+
+        public void WriteUnlock()
+        {
+            int lockCount = --_writeCount;
+            if (lockCount == 0)
+                Interlocked.Exchange(ref _flag, EMPTY_FLAG);
+            // 다 0으로 밀어준다 초기화
+        }
+
+        public void ReadLock() 
+        {
+            // 동일 쓰레드가 WriteLock을 이미 획득하고 있는지 확인
+            int lockThreadId = (_flag & WRITE_MASK) >> 16;
+            if (Thread.CurrentThread.ManagedThreadId == lockThreadId)
+            {
+                Interlocked.Increment(ref _flag);
+                return;
+            }
+
+            // 아무도 WriteLock을 획득하고 있지 않으면, ReadCount를 1 늘린다
+            while (true) 
+            {
+                for (int i =0; i < MAX_SPIN_COUNT; i++)
+                {
+                    int expected = (_flag & READ_MASK);
+                    if (Interlocked.CompareExchange(ref _flag, expected + 1, expected) == expected)
+                        return;
+                    // 의사 코드
+                    //if ((_flag & WRITE_MASK) == 0)
+                    //{
+                    //    _flag = _flag + 1;
+                    //    return;
+                    //}
+                    // A(0->1) B(0->1) 둘이 동시에 0에서 1로 바꿔달라고 요청
+                    // B(0->1) A가 먼저 성사됨 _flag가 1로 바뀐상태라 B는 실패
+                }
+                Thread.Yield();
+            }
+        }
+
+        public void ReadUnlock()
+        {
+            Interlocked.Decrement(ref _flag); // 1을 줄여준다
+        }
+
+        // 재귀적 락을 허용할지 (No)
+        // 스핀락 정책 (5000번 -> Yield)
+        //class Lock
+        //{
+        //    const int EMPTY_FLAG = 0x00000000;
+        //    const int WRITE_MASK = 0x7FFF0000;
+        //    const int READ_MASK = 0x0000FFFF;
+        //    const int MAX_SPIN_COUNT = 5000;
+
+        //    // 32비트
+        //    // [Unused(1)] [WriteThreadId(15)] [ReadCount(16)]
+        //    int _flag = EMPTY_FLAG;
+
+        //    public void WriteLock()
+        //    {
+        //        // 아무도 WriteLock or ReadLock을 획득하고 있지 않을 때, 경합해서 소유권을 얻는다
+        //        int desired = (Thread.CurrentThread.ManagedThreadId << 16) & WRITE_MASK;
+        //        while (true)
+        //        {
+        //            for (int i = 0; i < MAX_SPIN_COUNT; i++)
+        //            {
+        //                if (Interlocked.CompareExchange(ref _flag, desired, EMPTY_FLAG) == EMPTY_FLAG)
+        //                    return;
+        //                // 시도를 해서 성공하면 return
+        //                // 의사코드
+        //                // if (_flag == EMPTY_FLAG)                    
+        //                //    _flag = desired;                    
+        //            }
+
+        //            Thread.Yield();
+        //        }
+        //    }
+
+        //    public void WriteUnlock()
+        //    {
+        //        Interlocked.Exchange(ref _flag, EMPTY_FLAG);
+        //        // 다 0으로 밀어준다 초기화
+        //    }
+
+        //    public void ReadLock()
+        //    {
+        //        // 아무도 WriteLock을 획득하고 있지 않으면, ReadCount를 1 늘린다
+        //        while (true)
+        //        {
+        //            for (int i = 0; i < MAX_SPIN_COUNT; i++)
+        //            {
+        //                int expected = (_flag & READ_MASK);
+        //                if (Interlocked.CompareExchange(ref _flag, expected + 1, expected) == expected)
+        //                    return;
+        //                // 의사 코드
+        //                //if ((_flag & WRITE_MASK) == 0)
+        //                //{
+        //                //    _flag = _flag + 1;
+        //                //    return;
+        //                //}
+        //                // A(0->1) B(0->1) 둘이 동시에 0에서 1로 바꿔달라고 요청
+        //                // B(0->1) A가 먼저 성사됨 _flag가 1로 바뀐상태라 B는 실패
+        //            }
+        //            Thread.Yield();
+        //        }
+        //    }
+
+        //    public void ReadUnlock()
+        //    {
+        //        Interlocked.Decrement(ref _flag); // 1을 줄여준다
+        //    }
+    }
+}
--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -5,65 +5,39 @@ using System.Threading.Tasks;
 namespace ServerCore
 {
     class Program
-    { 
-        // 1. 근성
-        // 2. 양보
-        // 3. 갑질
-
-        // 상호 배제
-        // Monitor
-        static object _lock = new object();
-        static SpinLock _lock2 = new SpinLock();
-        static Mutex _lock3 = new Mutex(); // 위의 2개보다 무겁다
-
-        // 예를 들어 보상을 추가한다고 할떄
-        // [] [] [] [] []
-        class Reward
-        {
-
-        }
-
-        // RWLock ReadweWriteLock
-        static ReaderWriterLockSlim _lock4 = new ReaderWriterLockSlim();
-
-        // 99.9999%
-        static Reward GetRewardById(int id)
-        {
-            _lock4.EnterReadLock();
-
-            _lock4.ExitReadLock();
-
-            return null;
-        }
-        
-        // 0.0001%
-        // 일주일에 한번 호출될까 말까
-        static void AddReward(Reward reward)
-        {
-            _lock4.EnterWriteLock();
-            _lock4.ExitWriteLock();
-        }
-
+    {
+        static volatile int count = 0;
+        static Lock _lock = new Lock();
 
         static void Main(string[] args)
         {
-            lock (_lock)
+            Task t1 = new Task(delegate ()
             {
-
-            }
-
-            bool lockTaken = false;
-            try
-            {
-                _lock2.Enter(ref lockTaken);
-            }
-            finally
-            {
-                if (lockTaken)
+                for (int i = 0; i < 100000; i++)
                 {
-                    _lock2.Exit();
+                    _lock.WriteLock();
+                    _lock.WriteLock();
+                    count++;
+                    _lock.WriteUnlock();
+                    _lock.WriteUnlock();
                 }
-            }
+            });
+            Task t2 = new Task(delegate ()
+            {
+                for (int i = 0; i < 100000; i++)
+                {
+                    _lock.WriteLock();
+                    count--;
+                    _lock.WriteUnlock();
+                }
+            });
+
+            t1.Start();
+            t2.Start();
+
+            Task.WaitAll(t1, t2);
+
+            Console.WriteLine(count);
         }
     }
 }
