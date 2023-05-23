133142e81f1a596af95e3d595726688fdb0dcbd3
dokilove2@gmail.com <tulmooner2@>
2023-05-23 19:22:58 +0900

    섹션 1 : Thread Local Storage

--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -6,38 +6,28 @@ namespace ServerCore
 {
     class Program
     {
-        static volatile int count = 0;
-        static Lock _lock = new Lock();
+        static ThreadLocal<string> ThreadName = new ThreadLocal<string>(() => { return $"My Name is {Thread.CurrentThread.ManagedThreadId}"; });
+
+        static void WhoAmI()
+        {
+            bool repeat = ThreadName.IsValueCreated;
+
+            if (repeat)
+            {
+                Console.WriteLine(ThreadName.Value + " (repead)");
+            }
+            else
+            { Console.WriteLine(ThreadName.Value); }
+
+        }
 
         static void Main(string[] args)
         {
-            Task t1 = new Task(delegate ()
-            {
-                for (int i = 0; i < 100000; i++)
-                {
-                    _lock.WriteLock();
-                    _lock.WriteLock();
-                    count++;
-                    _lock.WriteUnlock();
-                    _lock.WriteUnlock();
-                }
-            });
-            Task t2 = new Task(delegate ()
-            {
-                for (int i = 0; i < 100000; i++)
-                {
-                    _lock.WriteLock();
-                    count--;
-                    _lock.WriteUnlock();
-                }
-            });
+            ThreadPool.SetMinThreads(1, 1);
+            ThreadPool.SetMaxThreads(3, 3);
+            Parallel.Invoke(WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI);
 
-            t1.Start();
-            t2.Start();
-
-            Task.WaitAll(t1, t2);
-
-            Console.WriteLine(count);
+            ThreadName.Dispose();
         }
     }
 }
