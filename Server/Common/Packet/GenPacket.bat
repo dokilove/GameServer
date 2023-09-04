bcdfd34d53ed76ca07ddd99b80895d23683f6fd2
dokilove2@gmail.com <tulmooner2@>
2023-09-04 19:04:21 +0900

    섹션 3 Packet Generator #6

--- Server/Common/Packet/GenPacket.bat
+++ Server/Common/Packet/GenPacket.bat
@@ -1,3 +1,5 @@
 START ../../PacketGanerator/bin/Debug/PacketGenerator.exe ../../PacketGanerator/PDL.xml
 XCOPY /Y GenPacket.cs "../../DummyClient/Packet"
 XCOPY /Y GenPacket.cs "../../Server/Packet"
+XCOPY /Y ClientPacketManager.cs "../../DummyClient/Packet"
+XCOPY /Y ServerPacketManager.cs "../../Server/Packet"
