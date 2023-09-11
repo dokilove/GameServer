71576942d4444855d36443ecbef2ff3d32ed034f
dokilove2@gmail.com <tulmooner2@>
2023-09-11 19:19:41 +0900

    섹션4 채팅테스트 #1

--- Server/Server/Program.cs
+++ Server/Server/Program.cs
@@ -12,7 +12,7 @@ namespace Server
     class Program
     {
         static Listener _listener = new Listener();
-
+        public static GameRoom Room = new GameRoom();
         static void Main(string[] args)
         {
             PacketManager.Instance.Register();
@@ -24,7 +24,7 @@ namespace Server
             IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
 
 
-            _listener.Init(endPoint, () => { return new ClientSession(); });
+            _listener.Init(endPoint, () => { return SessionManager.Instance.Generate(); });
             Console.WriteLine("Listening...");
 
             while (true)
